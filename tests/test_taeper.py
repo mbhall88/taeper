2bbd7a804d2e18c07fd5f0cc26b14a140250cb4f
mbhall88 <mbhall88@gmail.com>
2018-03-29 23:56:21 +0100

    test for scantree

--- tests/test_taeper.py
+++ tests/test_taeper.py
@@ -16,6 +16,7 @@ class TestZuluToEpochTime(unittest.TestCase):
 class TestExtractTimeFields(unittest.TestCase):
     """Test function that extracts time info from fast5 files"""
     def test_Read9Fast5TestFile_CorrectFieldsExtracted(self):
+        """Test fields in read9"""
         test_fast5 = 'tests/data/pass/read9.fast5'
         result = taeper.extract_time_fields(test_fast5)
         expected = {
@@ -25,3 +26,24 @@ class TestExtractTimeFields(unittest.TestCase):
             'start_time': 28238530.0
         }
         self.assertDictEqual(result, expected)
+
+
+class TestScantree(unittest.TestCase):
+    """Test scantree functiion"""
+    def test_TestData_FailReadsFollowedByPass(self):
+        """Test on tests/data directory"""
+        result = [direntry.path for direntry in taeper.scantree('tests/data')]
+        expected = [
+            'tests/data/fail/read0.fast5',
+            'tests/data/fail/read6.fast5',
+            'tests/data/pass/read1.fast5',
+            'tests/data/pass/read2.fast5',
+            'tests/data/pass/read3.fast5',
+            'tests/data/pass/read4.fast5',
+            'tests/data/pass/read5.fast5',
+            'tests/data/pass/read7.fast5',
+            'tests/data/pass/read8.fast5',
+            'tests/data/pass/read9.fast5'
+            ]
+        for x, y in zip(expected, result):
+            self.assertEqual(x, y)
