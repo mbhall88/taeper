bc47e2d038c51536522762fabcfe6d20d8f8817b
mbhall88 <mbhall88@gmail.com>
2018-03-29 23:45:47 +0100

    test for time field extraction

--- tests/test_taeper.py
+++ tests/test_taeper.py
@@ -4,13 +4,24 @@ from taeper import taeper
 
 
 class TestZuluToEpochTime(unittest.TestCase):
-    """Tests for `taeper` package."""
-
+    """Test Zulu to Epoch converter function."""
     def test_ExampleFromRead_CorrectTimeInSeconds(self):
-        """Set up test fixtures, if any."""
+        """Simple test case"""
         zulu_time = "2018-01-03T16:45:30Z"
         result = taeper._zulu_to_epoch_time(zulu_time)
         expected = 1514997930.0
         self.assertEqual(result, expected)
 
 
+class TestExtractTimeFields(unittest.TestCase):
+    """Test function that extracts time info from fast5 files"""
+    def test_Read9Fast5TestFile_CorrectFieldsExtracted(self):
+        test_fast5 = 'tests/data/pass/read9.fast5'
+        result = taeper.extract_time_fields(test_fast5)
+        expected = {
+            'exp_start_time': 1514997930.0,
+            'sampling_rate': 4000.0,
+            'duration': 19922.0,
+            'start_time': 28238530.0
+        }
+        self.assertDictEqual(result, expected)
