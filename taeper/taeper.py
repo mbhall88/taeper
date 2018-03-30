5782f64c61911805d63d2569527fa3b0709f0de8
mbhall88 <mbhall88@gmail.com>
2018-03-30 17:24:07 +0100

    test index generation

--- taeper/taeper.py
+++ taeper/taeper.py
@@ -38,12 +38,21 @@ def extract_time_fields(filepath: str) -> dict:
     """
     fast5_info = fast5.Fast5Info(filepath)
     fast5_file = fast5.Fast5File(filepath)
-    exp_start_time = fast5_file.get_tracking_id()['exp_start_time']
-    sampling_rate = float(fast5_file.get_channel_info()['sampling_rate'])
+    exp_start_time = fast5_file.get_tracking_id().get('exp_start_time')
+    sampling_rate = fast5_file.get_channel_info().get('sampling_rate')
+
+    if sampling_rate is None:
+        logging.warning(" {} missing 'sampling_rate' field. "
+                        "Skipping...".format(filepath))
+        return {}
+    if exp_start_time is None:
+        logging.warning(" {} missing 'exp_start_time' field. "
+                        "Skipping...".format(filepath))
+        return {}
 
     fields = {
         'exp_start_time': _zulu_to_epoch_time(exp_start_time),
-        'sampling_rate': sampling_rate,
+        'sampling_rate': float(sampling_rate),
         'duration': float(fast5_info.read_info[0].duration),
         'start_time': float(fast5_info.read_info[0].start_time)
     }
@@ -58,6 +67,10 @@ def calculate_timestamp(filepath: str) -> float:
     :returns Epoch time that the read finished sequencing
     """
     time_info = extract_time_fields(filepath)
+
+    if time_info == {}:  # missing field(s) in fast5 file
+        return 0.0
+
     experiment_start = time_info['exp_start_time']
 
     # adjust for the sampling rate of the channel
@@ -133,6 +146,55 @@ def check_positive(val):
     return val
 
 
+def get_timestamp_for_path(filepath: str) -> List:
+    """Gathers the timestamp for a file and returns it paired with the path.
+
+    :param filepath: Path to file.
+    :return: A list whose first element is the timestamp and second element
+    is the filepath. Returns an empty list if there is an issue with the file.
+    """
+    try:
+        timestamp = calculate_timestamp(filepath)
+        if timestamp == 0:  # missing field(s) in fast5 file
+            return []
+        time_path_pair = [timestamp, filepath]
+        logging.debug(time_path_pair)
+        return time_path_pair
+    except OSError as err:
+        logging.warning(" {} not processed. Error "
+                        "encountered: {}\n".format(filepath, err))
+        return []
+
+
+def filter_list(unfiltered_list: List) -> List:
+    """Filters empty lists and None's out of a list.
+
+    :param unfiltered_list: A list to filter.
+    :return: A list containing no empty lists or None's
+    """
+    filtered_list = [element for element in unfiltered_list
+                     if element != [] and element is not None]
+    return filtered_list
+
+
+def centre_list(uncentred_list: List[List]) -> List[Tuple]:
+    """Takes a list of lists and centres is on zero. That is each element is
+    turned into the difference between it and the previous element.
+
+    :param uncentred_list: A sorted list.
+    :return: A list with first element 0 and each element the difference to the
+    previous one.
+    """
+    # unzip the list in order to use numpy ediff1d method
+    timestamps, paths = zip(*uncentred_list)
+
+    # make the first read "time 0" and all others relative to that
+    zero_centered_times = np.ediff1d(timestamps, to_begin=0)
+
+    # zip times back with paths
+    return list(zip(zero_centered_times.round(decimals=3), paths))
+
+
 def generate_index(input_dir: str) -> List[Tuple[float, str]]:
     """Returns a list that is sorted in ascending order by time.
     All timepoints are relative to the first entry which is time 0.
@@ -143,32 +205,29 @@ def generate_index(input_dir: str) -> List[Tuple[float, str]]:
 
     """
     fast5_paths = scantree(input_dir, EXTENSION)
-    paths_with_their_timestamps = []
-    for f5_path in fast5_paths:
-        try:
-            timestamp = calculate_timestamp(f5_path)
-            time_path_pair = [timestamp, f5_path]
-            logging.debug(time_path_pair)
-            paths_with_their_timestamps.append(time_path_pair)
-        except OSError as err:
-            logging.warning(" {} not processed. Error "
-                            "encountered: {}\n".format(f5_path, err))
+    paths_with_their_timestamps = [get_timestamp_for_path(filepath)
+                                   for filepath in fast5_paths]
+
+    filtered_list = filter_list(paths_with_their_timestamps)
+
+    if len(filtered_list) == 0:
+        logging.error(" List of timestamps is empty. This likely means there "
+                      "are missing fields in your fast5 files.")
+        return []
 
     # todo: benchmark other sorting algorithms
-    paths_with_their_timestamps.sort()
+    filtered_list.sort()
 
-    # unzip the list in order to use numpy ediff1d method
-    timestamps, paths = zip(*paths_with_their_timestamps)
-    # make the first read "time 0" and all others relative to that
-    zero_centered_times = np.ediff1d(timestamps, to_begin=0)
-    # zip times back with paths
-    return list(zip(zero_centered_times.round(decimals=3), paths))
-   
+    zero_centred_list = centre_list(filtered_list)
+    return zero_centred_list
 
 
 def main(args):
     if args.input_dir:
-        centred_list = generate_index(args.input_dir)
+        index_list = generate_index(args.input_dir)
+        if not index_list:  # list is empty
+            logging.error(" Empty index. Exiting...")
+            return
         # np.save('file_order.npy', centred_list)
     else:
         # centred_list = open_pickle(args.input_pickle)
@@ -181,9 +240,9 @@ def main(args):
 
     # this will be subtracted from the time for each read
     prev_time = 0
-    for i, (delay, filepath) in enumerate(centred_list):
+    for i, (delay, filepath) in enumerate(index_list):
         delay = float(delay)
-        perc = round(float(i) / len(centred_list) * 100, 1)
+        perc = round(float(i) / len(index_list) * 100, 1)
         read_deposit(delay, prev_time, filepath, args.output_dir, args.scale)
         prev_time = delay
         print(">>> {}% of files transfered...".format(perc), end='\r')
--- tests/test_taeper.py
+++ tests/test_taeper.py
@@ -27,6 +27,12 @@ class TestExtractTimeFields(unittest.TestCase):
         }
         self.assertDictEqual(result, expected)
 
+    def test_OldFast5File_EmptyDict(self):
+        test_fast5 = 'tests/data/old.fast5'
+        result = taeper.extract_time_fields(test_fast5)
+        expected = {}
+        self.assertDictEqual(result, expected)
+
 
 class TestCalculateTimestamp(unittest.TestCase):
     """Make sure timestamps are calculated correctly"""
@@ -34,6 +40,12 @@ class TestCalculateTimestamp(unittest.TestCase):
         test_fast5 = 'tests/data/pass/read8.fast5'
         result = taeper.calculate_timestamp(test_fast5)
         expected = 1515004995.93975  # calculated by hand
+        self.assertEqual(result, expected)
+
+    def test_OldFast5_Zero(self):
+        test_fast5 = 'tests/data/old.fast5'
+        result = taeper.calculate_timestamp(test_fast5)
+        expected = 0
         self.assertEqual(result, expected)
 
 
@@ -48,6 +60,8 @@ class TestScantree(unittest.TestCase):
             'tests/data/fail/empty.fast5',
             'tests/data/fail/read0.fast5',
             'tests/data/fail/read6.fast5',
+            'tests/data/old.fast5',
+            'tests/data/pass/random.fast5',
             'tests/data/pass/read1.fast5',
             'tests/data/pass/read2.fast5',
             'tests/data/pass/read3.fast5',
@@ -68,3 +82,78 @@ class TestScantree(unittest.TestCase):
         expected = ['tests/data/corner.case', 'tests/data/fail/corner.case']
         for x, y in zip(expected, result):
             self.assertEqual(x, y)
+
+
+class TestFilterList(unittest.TestCase):
+    """Test filter list function"""
+    def test_ListWithNoNoneOrEmptyList_NoChange(self):
+        xs = [1, 2, 3]
+        result = taeper.filter_list(xs)
+        expected = [1, 2, 3]
+        self.assertListEqual(result, expected)
+
+    def test_ListWithNoneButNoEmptyList_FilterOutNone(self):
+        xs = [1, 2, None, 3]
+        result = taeper.filter_list(xs)
+        expected = [1, 2, 3]
+        self.assertListEqual(result, expected)
+
+    def test_ListWithNoNoneButEmptyList_FilterOutEmptyList(self):
+        xs = [1, 2, [], 3]
+        result = taeper.filter_list(xs)
+        expected = [1, 2, 3]
+        self.assertListEqual(result, expected)
+
+    def test_ListWithNoneAndEmptyList_FilterOutEmptyListAndNone(self):
+        xs = [1, None, 2, [], 3]
+        result = taeper.filter_list(xs)
+        expected = [1, 2, 3]
+        self.assertListEqual(result, expected)
+
+    def test_EmptyList_ReturnEmptyList(self):
+        xs = []
+        result = taeper.filter_list(xs)
+        expected = []
+        self.assertListEqual(result, expected)
+
+    def test_ListWithOnlyNone_ReturnEmptyList(self):
+        xs = [None]
+        result = taeper.filter_list(xs)
+        expected = []
+        self.assertListEqual(result, expected)
+
+    def test_ListWithOnlyEmptyList_ReturnEmptyList(self):
+        xs = [[]]
+        result = taeper.filter_list(xs)
+        expected = []
+        self.assertListEqual(result, expected)
+
+
+class TestCentreList(unittest.TestCase):
+    """Test centre list function"""
+    def test_GeneralCase(self):
+        xs = [[4, 'a'], [7, 'b'], [10, 'c']]
+        result = taeper.centre_list(xs)
+        expected = [(0, 'a'), (3, 'b'), (3, 'c')]
+        self.assertListEqual(result, expected)
+
+
+class TestGenerateIndex(unittest.TestCase):
+    """Test the function that generates the index"""
+    def test_TestFast5Files(self):
+        test_dir = 'tests/data'
+        result = taeper.generate_index(test_dir)
+        expected = [
+            (0.0, 'tests/data/pass/random.fast5'),
+            (24839288.405, 'tests/data/pass/read7.fast5'),
+            (7.476, 'tests/data/pass/read9.fast5'),
+            (1.327, 'tests/data/pass/read8.fast5'),
+            (6.065, 'tests/data/pass/read3.fast5'),
+            (18.221, 'tests/data/pass/read2.fast5'),
+            (34.035, 'tests/data/fail/read6.fast5'),
+            (3.294, 'tests/data/pass/read1.fast5'),
+            (0.126, 'tests/data/pass/read5.fast5'),
+            (8.73, 'tests/data/fail/read0.fast5'),
+            (0.235, 'tests/data/pass/read4.fast5')
+        ]
+        self.assertListEqual(result, expected)
